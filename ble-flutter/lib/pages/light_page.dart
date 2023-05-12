bf3461c6211fff8ced592a3c19f83eb3f3c58293
Göktuğ Furkan Arıca <67566490+Skyaigrette@users.noreply.github.com>
2023-05-12 14:31:38 +0300

    Update light_page.dart

--- ble-flutter/lib/pages/light_page.dart
+++ ble-flutter/lib/pages/light_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import '../home_page.dart';
 import '../main.dart';
 import '../mqtt_client_wrapper.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class LightPage extends StatefulWidget {
   const LightPage({Key? key}) : super(key: key);
@@ -10,21 +12,47 @@ class LightPage extends StatefulWidget {
 }
 
 class _LightPageState extends State<LightPage> {
+  int light = 0; //0
+  String? actualValue = "";
+  int thresholdValue = 0; //0
+  double _sliderValue = 0;
+  String? state = "";
   MQTTClientWrapper mqttClientWrapper = MQTTClientWrapper();
 
   @override
   void initState() {
     super.initState();
-    mqttClientWrapper.prepareMqttClient("", "", "192.168.0.108", 1883);
+    mqttClientWrapper.prepareMqttClient("", "", mqttIp, 1883);
 
     // wait for the client to connect
     Future.delayed(const Duration(seconds: 1)).then((_) {
       print("subscribing...");
-      mqttClientWrapper.subscribeToTopic("light$uuid")?.listen((message) {
+      mqttClientWrapper.subscribeToTopic("light\\$uuid")?.listen((message) {
         setState(() {
           print("Message: " + message);
 
           // Add the logic to parse the mqtt messages here
+          List<String> messageList = message.split('/');
+
+          actualValue = messageList[0];
+
+          if (actualValue![0] == "T") {
+            null;
+          } else {
+            light = int.parse(actualValue!);
+            thresholdValue = int.parse(messageList[1]);
+            state = messageList[2];
+            _sliderValue = thresholdValue / 36;
+            if (state == "1") {
+              state = "ON";
+            } else if (state == "0") {
+              state = "OFF";
+            }
+
+            print("Light: $light");
+            print('Threshold Value: $thresholdValue');
+            print('State: $state');
+          }
         });
       });
     });
@@ -70,30 +98,100 @@ class _LightPageState extends State<LightPage> {
                 ],
               ),
               Expanded(
-                  child: ListView(
-                physics: const BouncingScrollPhysics(),
-                children: const [
-                  SizedBox(height: 32),
-                  Center(
-                    child: Text(
-                      "data",
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    const SizedBox(height: 32),
+                    CircularPercentIndicator(
+                      radius: 160,
+                      lineWidth: 14,
+                      animateFromLastPercent: true,
+                      percent: thresholdValue / 3600, // THIS MIGHT NOT WORK
+                      progressColor: Colors.indigo,
+                      center: Column(
+                        children: [
+                          Text(
+                            // light -> gelen ışık verisi || threshold -> threshold verisi
+                            'Light: %${(light / 36).toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            // light -> gelen ışık verisi || threshold -> threshold verisi
+                            'Threshold: %${(thresholdValue / 36).toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            // light -> gelen ışık verisi || threshold -> threshold verisi
+                            'State: $state',
+                            style: const TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  SizedBox(height: 24),
-                  Center(
-                    child: Text(
-                      'LIGHT',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black54),
+                    const SizedBox(height: 24),
+                    const Center(
+                      child: Text(
+                        'LIGHT',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.black54),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 32),
-                ],
-              )),
+                    const SizedBox(height: 32),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 18),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 24),
+                            child: Text(
+                              'BRIGHTNESS',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Slider(
+                                value: _sliderValue,
+                                label: _sliderValue.round().toString(),
+                                max: 100,
+                                divisions: 100,
+                                onChanged: (double value) {
+                                  setState(() {
+                                    _sliderValue = value;
+                                    thresholdValue =
+                                        (_sliderValue * 36).toInt();
+                                    mqttClientWrapper.publishMessage(
+                                        'T$thresholdValue',
+                                        'light\\$uuid');
+                                  });
+                                },
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
