709ff359d09fd4528b927fe8c869bb5cffc56cde
Deniz <seadhy@protonmail.com>
2023-06-15 10:42:15 +0300

    Update api.py

--- fast_api/api.py
+++ fast_api/api.py
@@ -1,6 +1,7 @@
 import os
 import uvicorn
-import pyarrow.parquet as pq
+import pandas as pd
+from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import RedirectResponse
@@ -18,8 +19,8 @@ def create_exception(error_name: str, error_description: str) -> dict:
 class RequestPayload(BaseModel):
     uuid: str
     data_type: str
-    start_timestamp: int
-    stop_timestamp: int = 0
+    start_timestamp: Optional[int] = 0
+    stop_timestamp: Optional[int] = 0
 
 
 app = FastAPI()
@@ -39,39 +40,45 @@ async def get_data(request_payload: RequestPayload) -> dict:
     _start_timestamp = _request_payload['start_timestamp']
     _stop_timestamp = _request_payload['stop_timestamp']
 
+    print(_start_timestamp, _stop_timestamp)
+
     if not (_request_payload['uuid'] in _uuid_list):
         return create_exception('BAD_UUID', 'Please provide a UUID value that is in use.')
 
     if not (_request_payload['data_type'] in ('light', 'temp', 'humidity', 'motion')):
         return create_exception('BAD_DATA_TYPE', 'Please provide a valid data type.')
 
-    if len(str(_start_timestamp)) != 13:
+    if _start_timestamp and len(str(_start_timestamp)) != 13:
         return create_exception('BAD_START_TIMESTAMP', 'Please provide start_timestamp value in milliseconds.')
 
     if _stop_timestamp and len(str(_stop_timestamp)) != 13:
-        return create_exception('BAD_START_TIMESTAMP', 'Please provide stop_timestamp value in milliseconds.')
+        return create_exception('BAD_STOP_TIMESTAMP', 'Please provide stop_timestamp value in milliseconds.')
 
-    table = pq.read_table(f"db/{_request_payload['uuid']}/{_request_payload['data_type']}.parquet")
-
-    # data_frame = table.to_pandas()
+    # table = pq.read_table(f"db/{_request_payload['uuid']}/{_request_payload['data_type']}.parquet")
+    data_frame = pd.read_parquet(f"db/{_request_payload['uuid']}/{_request_payload['data_type']}.parquet")
 
     # print(data_frame.columns)
 
     # print(data_frame)
 
-    if _stop_timestamp:
-        filtered_columns = [col for col in table.schema.names if _start_timestamp <= int(col) <= _stop_timestamp]
+    if _start_timestamp and _stop_timestamp:
+        filtered_columns = [col for col in data_frame.columns if _start_timestamp <= int(col) <= _stop_timestamp]
+    elif _start_timestamp or _stop_timestamp:
+        if _start_timestamp:
+            filtered_columns = [col for col in data_frame.columns if _start_timestamp <= int(col)]
+        else:
+            filtered_columns = [col for col in data_frame.columns if int(col) <= _stop_timestamp]
     else:
-        filtered_columns = [col for col in table.schema.names if _start_timestamp <= int(col)]
+        filtered_columns = [col for col in data_frame.columns]
 
     # print(filtered_columns, len(filtered_columns))
 
     result_dict = {}
 
     for col in filtered_columns:
-        column_data = table[col].to_pandas().tolist()
+        column_data = data_frame[col].tolist()
         result_dict[col] = column_data
-
+    
     # print(result_dict)
 
     return result_dict
