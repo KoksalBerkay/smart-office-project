5ec4c8b35dd91f19ef481b68e10e1b6e59cf918e
KoksalBerkay <73394080+KoksalBerkay@users.noreply.github.com>
2023-06-19 11:56:20 +0300

    Update visualization_page.dart

--- ble-flutter/lib/pages/visualization_page.dart
+++ ble-flutter/lib/pages/visualization_page.dart
@@ -1,4 +1,68 @@
 import 'package:flutter/material.dart';
+import 'dashboard_page.dart';
+
+void processData(String visualData) {
+  // Split the visualData string by line breaks
+  List<String> lines = visualData.split('\n');
+
+  // Iterate through each line
+  for (String line in lines) {
+    // Remove leading and trailing whitespace from the line
+    line = line.trim();
+
+    // Skip empty lines
+    if (line.isEmpty) {
+      continue;
+    }
+
+    // Split the line by commas
+    List<String> values = line.split(',');
+
+    // Extract the individual values
+    double? data;
+    double? threshold;
+    String state = '';
+
+    for (String value in values) {
+      // Remove leading and trailing whitespace from each value
+      value = value.trim();
+
+      // Extract the data value
+      if (value.startsWith('Data:')) {
+        data = double.tryParse(value.split(':')[1]);
+      }
+      // Extract the threshold value
+      else if (value.startsWith('Threshold:')) {
+        threshold = double.tryParse(value.split(':')[1]);
+      }
+      // Extract the state value
+      else if (value.startsWith('State:')) {
+        state = value.split(':')[1].trim();
+      }
+    }
+
+    // Perform your desired control statements with the extracted values
+    if (data != null && threshold != null) {
+      // CONTROL STATEMENT
+      if (dataType == "light") {
+        data = double.parse((data / 40).toStringAsFixed(2));
+        threshold = double.parse((threshold / 40).toStringAsFixed(2));
+        print("Translated Data: $data, Threshold: $threshold, State: $state");
+      }
+      else if (dataType == "temp") {
+        print("Translated Data: $data, Threshold: $threshold, State: $state");
+      }
+      else if (dataType == "motion") {
+        data = double.parse((data / 60).toStringAsFixed(2));
+        threshold = double.parse((threshold / 60).toStringAsFixed(2));
+        print("Translated Data: $data, Threshold: $threshold, State: $state");
+      }
+      else if (dataType == "humidity") {
+        print("Translated Data: $data, Threshold: $threshold, State: $state");
+      }
+    }
+  }
+}
 
 class VisualizationPage extends StatefulWidget {
   const VisualizationPage({Key? key}) : super(key: key);
@@ -8,6 +72,12 @@ class VisualizationPage extends StatefulWidget {
 }
 
 class _VisualizationPageState extends State<VisualizationPage> {
+
+  void initState() {
+    super.initState();
+    processData(visualData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +105,10 @@ class _VisualizationPageState extends State<VisualizationPage> {
                   ],
                 ),
                 // Add more widgets here
+                const SizedBox(height: 32),
+                Center(child: Text("${dataType.toUpperCase()} DATA", style: const TextStyle(fontSize: 32))),
+                const SizedBox(height: 32),
+                Text(visualData),
               ],
             ),
           ),
