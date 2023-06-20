8228c732eb6d41e9926a86cf6e13ae670e676ddb
KoksalBerkay <73394080+KoksalBerkay@users.noreply.github.com>
2023-06-20 11:23:54 +0300

    unnecessary/old folder deleted

--- mqtt_client_flutter_main/android/app/src/main/kotlin/io/emqx/flutter_mqtt/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.emqx.flutter_mqtt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mqtt_client_flutter_main/android/app/src/main/kotlin/io/emqx/mqtt_client_flutter_main/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.emqx.mqtt_client_flutter_main
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
