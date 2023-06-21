abe227c0d6ddb4bfe8455702e95162ec9c9f37ab
Göktuğ Furkan Arıca <67566490+Skyaigrette@users.noreply.github.com>
2023-06-21 15:55:26 +0300

    Update pair.h
    yeah

--- espclient/pair.h
+++ espclient/pair.h
@@ -12,24 +12,27 @@ using namespace std;
 #define CHARACTERISTIC_SSID_UUID "4934c8ce-bce0-417c-b613-14f9f24da803"
 #define CHARACTERISTIC_PASS_UUID "7dec32af-0afe-4718-9c5b-a0c120bab609"
 #define CHARACTERISTIC_UUID_UUID "e91a0da9-9048-4b87-99a9-01a8a62b65bf"
+#define CHARACTERISTIC_MQTTPASS_UUID "340ab508-1009-11ee-be56-0242ac120002"
 #define ESP_FLASH_NAME "espflash"
 #define FLASH_WIFI_PASS "wifiPass"
 #define FLASH_WIFI_SSID "wifiSsid"
 #define FLASH_WIFI_UUID "uuid"
+#define FLASH_MQTT_PASS "mqttPass"
 
 
 String wifiPass;
 String wifiSsid;
 String _uuid;
+String _mqttPass; 
 Preferences pref;
 
 
 bool writeStringToFlash(String key, String value) {
   size_t valueSize = strlen(key.c_str());
-  
+
   pref.remove(key.c_str());
   size_t writtenSize = pref.putString(key.c_str(), value);
-  
+
   Serial.println("String saved into flash.");
   return valueSize == writtenSize ? true : false;
 }
@@ -62,7 +65,7 @@ class ServerCallback : public BLEServerCallbacks {
     void onDisconnect(BLEServer* pServer) {
       Serial.println("Client disconnected.");
       BLEDevice::startAdvertising();
-      
+
     }
 };
 
@@ -73,26 +76,32 @@ void startBluetooth() {
   pServer->setCallbacks(new ServerCallback());
 
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *ssidCharacteristic = pService->createCharacteristic(
-                         CHARACTERISTIC_SSID_UUID,
-                         BLECharacteristic::PROPERTY_WRITE);
-  BLECharacteristic *passCharacteristic = pService->createCharacteristic(
-                  CHARACTERISTIC_PASS_UUID,
-                  BLECharacteristic::PROPERTY_WRITE);
+  
   BLECharacteristic *uuidCharacteristic = pService->createCharacteristic(
-                  CHARACTERISTIC_UUID_UUID,
-                  BLECharacteristic::PROPERTY_READ);
+      CHARACTERISTIC_UUID_UUID,
+      BLECharacteristic::PROPERTY_READ);
+  BLECharacteristic *mqttPassCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_MQTTPASS_UUID,
+      BLECharacteristic::PROPERTY_READ);
+  BLECharacteristic *ssidCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_SSID_UUID,
+      BLECharacteristic::PROPERTY_WRITE);
+  BLECharacteristic *passCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_PASS_UUID,
+      BLECharacteristic::PROPERTY_WRITE);
 
   _uuid = StringUUIDGen();
-  uuidCharacteristic->setValue(string(_uuid.c_str())); 
+  _mqttPass = StringUUIDGen();
+  uuidCharacteristic->setValue(string(_uuid.c_str()));
+  mqttPassCharacteristic->setValue(string(_mqttPass.c_str()));
   Serial.println("UUID -> " + _uuid);
+  Serial.println("UUID PASS -> " + _mqttPass);
   ssidCharacteristic->setCallbacks(new CharacteristicCallback());
   passCharacteristic->setCallbacks(new CharacteristicCallback());
-  
   BLEAdvertisementData advertisementData;
   advertisementData.setManufacturerData(MANUFACTURER_NAME);
   advertisementData.setName("test_esp");
-  
+
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
@@ -102,45 +111,51 @@ void startBluetooth() {
   pService->start();
   BLEDevice::startAdvertising();
   delay(1000);
+  Serial.println("BT Started.");
 }
 
 
 
 void clearFlash() {
+  Serial.println("Flash cleared");
   pref.begin(ESP_FLASH_NAME, false);
   pref.clear();
   pref.end();
 }
 
 
-void Pair(String* ssid ,String* pass ,String* UUID ,bool reseteeprom = false ,unsigned int timeOut = -1) {
+void Pair(String* ssid , String* pass , String* UUID ,String* MQTTPASS , bool reseteeprom = false , unsigned int timeOut = -1) {
 
   pref.begin(ESP_FLASH_NAME);
   int time = millis();
-  
-  if(!reseteeprom){
+
+  if (!reseteeprom) {
     wifiSsid = pref.getString(FLASH_WIFI_SSID, "");
     wifiPass = pref.getString(FLASH_WIFI_PASS, "");
     _uuid = pref.getString(FLASH_WIFI_UUID, "");
+    _mqttPass = pref.getString(FLASH_MQTT_PASS, "");
+  }else{
+    clearFlash();
   }
 
 
-// mfmfmf2020!
+  // mfmfmf2020!
 
-  if (wifiSsid == 0 || wifiPass == 0 || _uuid == 0) {
-    
+  if (wifiSsid == 0 || wifiPass == 0 || _uuid == 0 || _mqttPass == 0) {
+
     startBluetooth();
-    while ((wifiSsid == 0 || wifiPass == 0) && (millis() - time) < timeOut) {                                                                                                                                                                                                                                                                                                                                                           
+    while ((wifiSsid == 0 || wifiPass == 0) && (millis() - time) < timeOut) {
       delay(50);
-      
+
     }
-    
+
     writeStringToFlash(FLASH_WIFI_SSID, wifiSsid);
     writeStringToFlash(FLASH_WIFI_PASS, wifiPass);
     writeStringToFlash(FLASH_WIFI_UUID, _uuid);
+    writeStringToFlash(FLASH_MQTT_PASS, _mqttPass);
 
   }
-
+   
   delay(1000);
   BLEDevice::stopAdvertising();
   BLEDevice::deinit();
@@ -149,4 +164,5 @@ void Pair(String* ssid ,String* pass ,String* UUID ,bool reseteeprom = false ,un
   *ssid = wifiSsid;
   *pass = wifiPass;
   *UUID = _uuid;
+  *MQTTPASS = _mqttPass;
 }
