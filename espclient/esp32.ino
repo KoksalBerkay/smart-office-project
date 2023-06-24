1a1514d6afeb1973c48f59092969eb89df83e42b
Maydas0 <119695402+Maydas0@users.noreply.github.com>
2023-06-24 14:30:53 +0300

    Update esp32.ino

--- espclient/esp32.ino
+++ espclient/esp32.ino
@@ -4,8 +4,8 @@
 #define PIR_SENSOR 15
 #define DHT_PIN 5
 #define LDR_PIN 35
-#define RELAY_PIN 26
-#define LIGHT_PIN 19
+#define RELAY_PIN 23
+#define LIGHT_PIN 22
 #define SERVER_IP "192.168.1.97"
 #define SERVER_PORT 1883
 #define ESPBUTTON 0
