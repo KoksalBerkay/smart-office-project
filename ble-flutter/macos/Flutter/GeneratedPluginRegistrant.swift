21b0a19240008f3a75c6f27fd05b9616a6df4986
KoksalBerkay <73394080+KoksalBerkay@users.noreply.github.com>
2024-06-24 13:36:44 +0300

    Project CleanUp and Documentation

--- ble-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ble-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import network_info_plus
+import shared_preferences_foundation
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
